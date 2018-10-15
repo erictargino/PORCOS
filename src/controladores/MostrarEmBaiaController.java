e160e31d3b6248cf835920570b723e2a26aa52b4
Heverton Gomes <Heverton Gomes@DESKTOP-ADK3O2P>
2018-10-15 06:47:32 -0300

    Telas com ligações okay, implementar agora parte do crud e visualização

--- src/controladores/MostrarEmBaiaController.java
+++ src/controladores/MostrarEmBaiaController.java
@@ -5,23 +5,59 @@
  */
 package controladores;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 
 /**
  * FXML Controller class
  *
- * @author Heverton
+ * @author Heverton Gomes
  */
 public class MostrarEmBaiaController implements Initializable {
+    
+    @FXML
+    private BorderPane borderPrincipal;
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TableView<?> tableIdePorcos;
+
+    @FXML
+    private TableColumn<?, ?> idPorcos;
+
+    @FXML
+    private TableColumn<?, ?> idBaias;
+
+    @FXML
+    private JFXButton btVoltar;
+
+ 
+     @FXML
+    private void voltarBaias(){
+        
+        try {
+            Parent baia = FXMLLoader.load(getClass().getResource("/visao/TelaTrocaBaia.fxml"));
+            borderPrincipal.setCenter(baia);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
     
+    
+    
 }
+
